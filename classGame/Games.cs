ddf5ff6747848a3c19c49c4e4c72d2e392ba8632
KuRaJ <Kuraj.com>
2026-03-17 13:30:18 +0800

    create repos

---
+++ classGame/Games.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classGame
+{
+    public class CS
+    {
+        public int Kill = 0;
+        public bool isWinner = true; 
+        public int RoundsMax = 0;
+    }
+
+    public class Dota
+    {
+        public int Kill = 0;
+        public bool isWinner = true;
+        public string Hero = "Puje";
+    }
+
+    public class Watermelon
+    {
+        public int Ripeness = 0;
+        public int BonesNumber = 0;
+        public bool HasStripes = false;
+    }
+}
